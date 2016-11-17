dab79cde5c99007521cb16bf744c56e4d64b767a
Aaron Marcher <info@nulltime.net>
2016-11-17 09:31:47 +0100

    Initial commit

---
+++ config.def.h
@@ -0,0 +1,11 @@
+/* See LICENSE file for copyright and license details. */
+
+static const unsigned int repeat = 60;
+
+static const signed int warning = 10;
+static const signed int critical = 5;
+static const signed int danger = 2;
+
+static const char dangercmd[] = "sudo zzz";
+
+static const char battery[] = "BAT0";
---
+++ juiced.c
@@ -0,0 +1,92 @@
+/* See LICENSE file for copyright and license details. */
+
+#include <err.h>
+#include <libnotify/notify.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#undef strlcat
+#undef strlcpy
+
+#include "extern/arg.h"
+#include "extern/strlcat.h"
+#include "extern/strlcpy.h"
+#include "extern/concat.h"
+
+#include "config.h"
+
+char *argv0;
+char concat[];
+static unsigned short int done;
+static unsigned short int dflag;
+
+int
+main(int argc, char *argv[])
+{
+	ARGBEGIN {
+		case 'd':
+			dflag = 1;
+			break;
+		case 'v':
+			printf("juiced %s (C) 2016 juiced engineers\n", VERSION);
+			return 0;
+		default:
+			fprintf(stderr, "usage: %s [-dhv]\n", argv0);
+			return 1;
+	} ARGEND
+
+	if (dflag && daemon(1, 1) < 0) {
+		err(1, "daemon");
+	}
+
+	while (!done) {
+		char state[12];
+		int perc;
+		FILE *percfile;
+		FILE *statefile;
+
+		ccat(3, "/sys/class/power_supply/", battery, "/status");
+		statefile = fopen(concat, "r");
+		if (statefile == NULL) {
+			warn("Failed to open file %s", concat);
+			return 1;
+		}
+		fscanf(statefile, "%12s", state);
+		fclose(statefile);
+
+		if (strcmp(state, "Discharging") == 0) {
+			ccat(3, "/sys/class/power_supply/", battery, "/capacity");
+			percfile = fopen(concat, "r");
+			if (percfile == NULL) {
+				warn("Failed to open file %s", concat);
+				return 1;
+			}
+			fscanf(percfile, "%i", &perc);
+			fclose(percfile);
+
+			if (perc <= warning && notify_init("juiced")) {
+				NotifyNotification *notification = notify_notification_new("Notice", "Battery is low.", NULL);
+				notify_notification_set_urgency(notification, NOTIFY_URGENCY_NORMAL);
+				notify_notification_show(notification, NULL);
+				g_object_unref(notification);
+				notify_uninit();
+			}
+
+			if (perc <= critical && notify_init("juiced")) {
+				NotifyNotification *notification = notify_notification_new("Warning", "Battery is critically low.", NULL);
+				notify_notification_set_urgency(notification, NOTIFY_URGENCY_NORMAL);
+				notify_notification_show(notification, NULL);
+				g_object_unref(notification);
+				notify_uninit();
+			}
+
+			if (perc <= danger) {
+				system(dangercmd);
+			}
+		}
+
+		sleep(repeat);
+	}
+
+	return 0;
+}
