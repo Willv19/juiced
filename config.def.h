ac923278be090fe4aacba37df8af713603161a84
Will Vauclain <willvauclain.dev@gmail.com>
2019-04-16 23:21:17 -0400

    Changed battery range to fix hibernate issue

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const unsigned int repeat = 60;
 /* order should be danger < critical < warning; numbers in percent; check is <= */
 static const signed int warning = 25;
 static const signed int critical = 10;
-static const signed int danger = 2;
+static const signed int danger = 3;
 
 /* what command to run if battery is under "danger" value */
 static const char dangercmd[] = "notify-send -u critical 'Critical' 'Hibernated system due to low battery' && systemctl hibernate";
